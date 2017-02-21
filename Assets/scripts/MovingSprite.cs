227a253efbfbffc4cee83cf2b3c65fd60d410237
aggienav63 <aggienav63@gmail.com>
2017-02-21 01:18:26 -0700

    Add files via upload

---
+++ Assets/scripts/MovingSprite.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A moving sprite
+/// </summary>
+public class MovingSprite : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        // STUDENTS: get the sprite moving by adding a force to the
+        // Rigidbody2D component. The x and y components of the force
+        // vector should be float random numbers between -4 and 4
+        // inclusive
+
+    }
+	
+}
---
+++ Assets/scripts/RandomMovingSprites.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Game manager
+/// </summary>
+public class RandomMovingSprites : MonoBehaviour {
+
+    [SerializeField]
+    GameObject prefabMovingSprite0;
+    [SerializeField]
+    GameObject prefabMovingSprite1;
+    [SerializeField]
+    GameObject prefabMovingSprite2;
+
+    // timing support
+    const float ChangeDelaySeconds = 1;
+    float elapsedSeconds = 0;
+	
+	// Update is called once per frame
+	void Update () {
+
+        elapsedSeconds += Time.deltaTime;
+        if (elapsedSeconds > ChangeDelaySeconds) {
+            elapsedSeconds = 0;
+
+            // destroy current moving sprite
+            GameObject currentMovingSprite = GameObject.FindGameObjectWithTag("Moving Sprite");
+            if (currentMovingSprite != null) {
+                Destroy(currentMovingSprite);
+            }
+
+            // STUDENTS: uncomment the code below and make it generate a random number 
+            // between 0 and 2 inclusive using the UnityEngine Random class and 
+            // appropriate type casting
+            int prefabNumber = Random.Range(0,2);
+
+            // STUDENTS: uncomment the lines below and implement
+            // the required code
+            if (prefabNumber == 0) {
+
+                Instantiate(prefabMovingSprite0);
+                //    // STUDENTS: instantiate a new instance of the prefab
+                //    // for MovingSprite0 at location(0, 0, 0)
+
+                } else if (prefabNumber == 1) {
+
+                //    // STUDENTS: instantiate a new instance of the prefab
+                //    // for MovingSprite1 at location(0, 0, 0)
+
+                Instantiate(prefabMovingSprite1);
+
+                } else if (prefabNumber == 2) {
+
+                //    // STUDENTS: instantiate a new instance of the prefab
+                //    // for MovingSprite1 at location(0, 0, 0)
+
+                Instantiate(prefabMovingSprite2);
+
+                }
+            }
+    }
+
+}
+
